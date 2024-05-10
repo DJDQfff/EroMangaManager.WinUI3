68cfd06881a0ac3dc536775ea9329619e4b0ac0e
DJDQfff <2743109998@qq.com>
2024-05-10 14:57:37 +0800

    bookcase和一些usercontrol使用mvvmtoolkit

--- EroMangaManager.WinUI3/UserControls/MangaBookBriefInfo.xaml.cs
+++ EroMangaManager.WinUI3/UserControls/MangaBookBriefInfo.xaml.cs
@@ -2,20 +2,11 @@
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace EroMangaManager.WinUI3.UserControls;
-
+[INotifyPropertyChanged]
 public sealed partial class MangaBookBriefInfo : UserControl
 {
+    [ObservableProperty]
     private MangaBook mangaBook;
-
-    public MangaBook MangaBook
-    {
-        get => mangaBook;
-        set
-        {
-            mangaBook = value;
-            this.Bindings.Update();
-        }
-    }
 
     public MangaBookBriefInfo ()
     {
--- EroMangaManager.WinUI3/UserControls/MangaBookCoverTags.xaml.cs
+++ EroMangaManager.WinUI3/UserControls/MangaBookCoverTags.xaml.cs
@@ -2,16 +2,11 @@
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace EroMangaManager.WinUI3.UserControls;
-
+[INotifyPropertyChanged]
 public sealed partial class MangaBookCoverTags : UserControl
 {
+    [ObservableProperty]
     private MangaBook mangaBook;
-
-    public MangaBook MangaBook
-    {
-        set { mangaBook = value; this.Bindings.Update(); }
-        get => mangaBook;
-    }
 
     public MangaBookCoverTags ()
     {
