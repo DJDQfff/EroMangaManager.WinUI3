da6c702cd62bf17dec2b951c216b3df780401622
DJDQfff <2743109998@qq.com>
2024-05-10 19:17:42 +0800

    添加一个漫画打开方式切换空间，bookcase页面的一个控件改用这个

---
+++ EroMangaManager.WinUI3/UserControls/MangaOpenButton.xaml.cs
@@ -0,0 +1,16 @@
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EroMangaManager.WinUI3.UserControls
+{
+    public sealed partial class MangaOpenButton : UserControl
+    {
+        public MangaOpenButton ()
+        {
+            this.InitializeComponent();
+        }
+
+
+    }
+}
