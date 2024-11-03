b32f4cde9017164513d13272fbf27382f3c60ae5
DJDQfff <2743109998@qq.com>
2024-11-03 16:05:10 +0800

    StringsExtension添加一个todo

--- EroMangaManager.WinUI3/Strings/StringsExtension.cs
+++ EroMangaManager.WinUI3/Strings/StringsExtension.cs
@@ -1,13 +1,35 @@
 ﻿namespace EroMangaManager.WinUI3.Strings;
+
 ///<summary> </summary>
 public class StringsExtension : MarkupExtension
 {
     ///<summary> </summary>
     public StringsEnum Uid { get; set; }
 
+    // TODO 设置英语模式下，大小写模式
+    //public UpperMode CharcaterMode { set; get; }
+
     ///<summary> </summary>
     protected override object ProvideValue ()
     {
         return ResourceLoader.GetForViewIndependentUse().GetString(Uid.ToString());
     }
+}
+
+public enum UpperMode
+{
+    /// <summary>
+    /// 首字母大写
+    /// </summary>
+    FirstUpper,
+
+    /// <summary>
+    /// 全大写
+    /// </summary>
+    AllUpper,
+
+    /// <summary>
+    /// 全小写
+    /// </summary>
+    AllLower
 }
