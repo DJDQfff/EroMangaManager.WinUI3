623543c47fbc7eae2b09e564b310a8738a2a94a0
DJDQfff <2743109998@qq.com>
2025-06-20 11:03:52 +0800

    数据库逆向小改动

---
+++ ReverseDownloadsData/OutputData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReserseDownloadsData;
+
+namespace ReverseDownloadsData;
+
+internal class OutputData
+{
+    internal void WritelineAuthor()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var list = new List<string>();
+        BookContext context = new BookContext();
+        var books = context.Books.ToList();
+        books.ForEach(x => Console.WriteLine(x.Title));
+        return;
+        var path = @"D:\test\ReserseDownloadsData\bin\Debug\net8.0\translator.txt";
+        var lines = File.ReadAllLines(path);
+        var lines2 = lines.Select(l => l.Trim()).Distinct().ToList();
+        File.WriteAllLines(
+            @"D:\test\ReserseDownloadsData\bin\Debug\net8.0\translator2.txt",
+            lines2
+        );
+        return;
+        var b = context.Books
+            .Select(x => x.ChineseTeam)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Distinct()
+            .ToList();
+        foreach (var item in b)
+        {
+            Console.WriteLine(item);
+        }
+        System.IO.File.WriteAllLines("translator.txt", b);
+
+        static IEnumerable<string> GetAuthor(List<string> list, BookContext context)
+        {
+            var a = context.Books.Select(x => x.Author).ToList();
+            foreach (var item in a)
+            {
+                var ss = item.Split('(', ')', ',', '[', ']', '、', '（', '）', '/', '\\', '．');
+                list.AddRange(ss);
+            }
+            var c = list.Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).Distinct();
+            System.IO.File.WriteAllLines("author.txt", c);
+            return c;
+        }
+    }
+}
--- ReverseDownloadsData/Program.cs
+++ ReverseDownloadsData/Program.cs
@@ -1,38 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ReserseDownloadsData;
 
-Console.WriteLine("Hello, World!");
+using ReverseDownloadsData;
 
-var list = new List<string>();
-BookContext context = new BookContext();
-var books = context.Books.ToList();
-books.ForEach(x => Console.WriteLine(x.Title));
-return;
-var path = @"D:\test\ReserseDownloadsData\bin\Debug\net8.0\translator.txt";
-var lines = File.ReadAllLines(path);
-var lines2 = lines.Select(l => l.Trim()).Distinct().ToList();
-File.WriteAllLines(@"D:\test\ReserseDownloadsData\bin\Debug\net8.0\translator2.txt" , lines2);
-return;
-var b = context.Books
-    .Select(x => x.ChineseTeam)
-    .Where(x => !string.IsNullOrWhiteSpace(x))
-    .Distinct()
-    .ToList();
-foreach (var item in b)
+var context = new BookContext();
+var favorites = context.Favorites.ToList();
+Console.WriteLine(favorites.Count);
+foreach (var favorite in favorites)
 {
-    Console.WriteLine(item);
-}
-System.IO.File.WriteAllLines("translator.txt" , b);
-
-static IEnumerable<string> GetAuthor (List<string> list , BookContext context)
-{
-    var a = context.Books.Select(x => x.Author).ToList();
-    foreach (var item in a)
+    var book = context.Books.SingleOrDefault(x => x.Id == favorite.Id);
+    if (book != null)
     {
-        var ss = item.Split('(' , ')' , ',' , '[' , ']' , '、' , '（' , '）' , '/' , '\\' , '．');
-        list.AddRange(ss);
+        Console.WriteLine("*********");
+
+        Console.WriteLine(book.Title);
+        Console.WriteLine(book.Description);
+        Console.WriteLine(book.Author);
     }
-    var c = list.Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).Distinct();
-    System.IO.File.WriteAllLines("author.txt" , c);
-    return c;
 }
